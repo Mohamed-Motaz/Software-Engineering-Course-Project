fe5f08a9b694911ef078de00d88566a020ccba25
Mohamed247 <mohamedmotaz247@yahoo.com>
2021-05-21 23:58:43 +0200

    add basic structure to project

--- Software-Engineering-Course-Project/Form1.Designer.cs
+++ Software-Engineering-Course-Project/Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.connectedBtn = new System.Windows.Forms.Button();
+            this.disconnectedBtn = new System.Windows.Forms.Button();
+            this.initializeDataBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // connectedBtn
+            // 
+            this.connectedBtn.Location = new System.Drawing.Point(232, 281);
+            this.connectedBtn.Name = "connectedBtn";
+            this.connectedBtn.Size = new System.Drawing.Size(91, 58);
+            this.connectedBtn.TabIndex = 0;
+            this.connectedBtn.Text = "Connected Mode";
+            this.connectedBtn.UseVisualStyleBackColor = true;
+            this.connectedBtn.Click += new System.EventHandler(this.connectedBtn_Click);
+            // 
+            // disconnectedBtn
+            // 
+            this.disconnectedBtn.Location = new System.Drawing.Point(403, 281);
+            this.disconnectedBtn.Name = "disconnectedBtn";
+            this.disconnectedBtn.Size = new System.Drawing.Size(91, 58);
+            this.disconnectedBtn.TabIndex = 1;
+            this.disconnectedBtn.Text = "Disconnected Mode";
+            this.disconnectedBtn.UseVisualStyleBackColor = true;
+            this.disconnectedBtn.Click += new System.EventHandler(this.disconnectedBtn_Click);
+            // 
+            // initializeDataBtn
+            // 
+            this.initializeDataBtn.Location = new System.Drawing.Point(261, 47);
+            this.initializeDataBtn.Name = "initializeDataBtn";
+            this.initializeDataBtn.Size = new System.Drawing.Size(167, 67);
+            this.initializeDataBtn.TabIndex = 2;
+            this.initializeDataBtn.Text = "Initialize All Data";
+            this.initializeDataBtn.UseVisualStyleBackColor = true;
+            this.initializeDataBtn.Click += new System.EventHandler(this.initializeDataBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.initializeDataBtn);
+            this.Controls.Add(this.disconnectedBtn);
+            this.Controls.Add(this.connectedBtn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button connectedBtn;
+        private System.Windows.Forms.Button disconnectedBtn;
+        private System.Windows.Forms.Button initializeDataBtn;
     }
 }
 
--- Software-Engineering-Course-Project/Form1.cs
+++ Software-Engineering-Course-Project/Form1.cs
@@ -7,14 +7,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
 
 namespace Software_Engineering_Course_Project
 {
     public partial class Form1 : Form
     {
+        private List<string> tableCreationScripts = new List<string>() 
+        { "drop table users", 
+            "drop table customers",
+            @"Create Table Users
+                (userID number(8)  primary key, isAdmin varchar2(1), userName varchar2(50), userEmail varchar2(50) unique)",
+            @"Create Table Customers
+                ( CustomerID number(8)  primary key,  CustomerName varchar2(50),  CustomerEmail varchar2(50) unique,  CustomerPlan varchar2(1))" };
+
+        private List<string> insertDataScripts = new List<string>() 
+        { @"insert into Users values
+            (1, 'y', 'Mohamed Ahmed', 'mohamed.ahmed34@gmail.com')",
+            @"insert into Users values
+            (2, 'y', 'Rawan Ahmed', 'rawan.ahmed4@gmail.com')",
+            @"insert into Users values
+            (3, 'n', 'Kareem Ezzat', 'kareemezzat20@gmail.com')",
+            @"insert into Users values
+            (4, 'n', 'Mohamed Motaz', 'mohamedmotar87@gmail.com')",
+            @"insert into Users values
+            (5, 'n', 'Ali Anwar', 'alianwar123@gmail.com')",
+            @"Insert into Customers values
+            (1,'Kristen Stewart', 'Kristen_Stewart@gmail.com','A')",
+            @"Insert into Customers values
+            (2,'Robert Pattinson', 'Robert_Pattinson@gmail.com','A')",
+            @"Insert into Customers values
+            (3,'Taylor Lautner', 'Taylor_Lautner@gmail.com','C')",
+            @"Insert into Customers values
+            (4,'Rupert Grint', 'Rupert_Grint@gmail.com','C')",
+            @"Insert into Customers values
+            (5,'Daniel Radcliffe', 'Daniel_Radcliffe@gmail.com','C')",
+            @"Insert into Customers values
+            (6,'Ralph Fiennes', 'Ralph_Fiennes@gmail.com','B')"
+        };
+
+        private string connectionString = "data source=orcl; user id=scott; password=tiger;";
+        OracleConnection conn;
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void connectedBtn_Click(object sender, EventArgs e)
+        {
+            ConnectedMode form = new ConnectedMode();
+            form.Show();
+        }
+
+        private void disconnectedBtn_Click(object sender, EventArgs e)
+        {
+            DisconnectedMode form = new DisconnectedMode();
+            form.Show();
+        }
+
+        private void initializeDataBtn_Click(object sender, EventArgs e)
+        {
+            conn = new OracleConnection(connectionString);
+            conn.Open();
+
+            foreach (string command in tableCreationScripts)
+            {
+                OracleCommand cmd = new OracleCommand();
+                cmd.Connection = conn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = command;
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }catch { }
+            }
+            foreach (string command in insertDataScripts)
+            {
+                OracleCommand cmd = new OracleCommand();
+                cmd.Connection = conn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = command;
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch { }
+            }
+            MessageBox.Show("All tables created and all data added succesfully");
+            conn.Dispose();
         }
     }
 }
