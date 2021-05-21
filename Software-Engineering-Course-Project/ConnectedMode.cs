3933c7b585e28c91169c8f70e76f1acc97305e70
Mohamed247 <mohamedmotaz247@yahoo.com>
2021-05-22 00:36:23 +0200

    add connected mode basics

--- Software-Engineering-Course-Project/ConnectedMode.cs
+++ Software-Engineering-Course-Project/ConnectedMode.cs
@@ -7,14 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
 
 namespace Software_Engineering_Course_Project
 {
     public partial class ConnectedMode : Form
     {
+        OracleConnection conn;
+        private string connectionString = "data source=orcl; user id=scott; password=tiger;";
         public ConnectedMode()
         {
             InitializeComponent();
+            conn = new OracleConnection(connectionString);
+            conn.Open();
+        }
+
+        private void ConnectedMode_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            conn.Dispose();
+        }
+
+        private void ConnectedMode_Load(object sender, EventArgs e)
+        {
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select userid from users";
+
+            OracleDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                textVersionComboBox.Items.Add(dr[0]);
+            }
+            dr.Close();
+        }
+
+        private void textVersionComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from users where userid=:id";
+            cmd.Parameters.Add("id", textVersionComboBox.SelectedItem.ToString());
+
+            OracleDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                userNameTxtBox.Text = dr["username"].ToString();
+                userEmailTxtBox.Text = dr["useremail"].ToString();
+                isAdminTxtBox.Text = dr["isAdmin"].ToString();
+            }
         }
     }
 }
