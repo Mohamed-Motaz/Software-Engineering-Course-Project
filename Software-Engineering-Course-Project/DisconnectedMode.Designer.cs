6c0dbb203661095c92fb3a6f7a7e2e71a7b7a521
KareemE125 <kareemezzat.125@gmail.com>
2021-05-25 00:47:08 +0200

    dis

--- Software-Engineering-Course-Project/DisconnectedMode.Designer.cs
+++ Software-Engineering-Course-Project/DisconnectedMode.Designer.cs
@@ -34,6 +34,8 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.radioUser = new System.Windows.Forms.RadioButton();
             this.radioCust = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.searchBox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -100,11 +102,30 @@
             this.radioCust.Text = "Customers Information";
             this.radioCust.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(120, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(94, 144);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(100, 22);
+            this.searchBox.TabIndex = 9;
+            // 
             // DisconnectedMode
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1098, 751);
+            this.Controls.Add(this.searchBox);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.saveBtn);
             this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.loadBtn);
@@ -117,6 +138,7 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -128,5 +150,7 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton radioUser;
         private System.Windows.Forms.RadioButton radioCust;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox searchBox;
     }
 }
