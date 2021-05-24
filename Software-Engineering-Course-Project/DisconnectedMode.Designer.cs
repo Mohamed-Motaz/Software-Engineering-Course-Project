d83fa2c10289c63a0b4ddad4ca450fa5c554a616
KareemE125 <kareemezzat.125@gmail.com>
2021-05-25 00:31:42 +0200

    disconnected

--- Software-Engineering-Course-Project/DisconnectedMode.Designer.cs
+++ Software-Engineering-Course-Project/DisconnectedMode.Designer.cs
@@ -28,33 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.loadBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioUser = new System.Windows.Forms.RadioButton();
+            this.radioCust = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // saveBtn
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(302, 64);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(138, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "This is the connected mode";
+            this.saveBtn.Location = new System.Drawing.Point(507, 696);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(108, 39);
+            this.saveBtn.TabIndex = 7;
+            this.saveBtn.Text = "SAVE";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(31, 239);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(1014, 434);
+            this.dataGridView.TabIndex = 6;
+            // 
+            // loadBtn
+            // 
+            this.loadBtn.Location = new System.Drawing.Point(449, 173);
+            this.loadBtn.Name = "loadBtn";
+            this.loadBtn.Size = new System.Drawing.Size(199, 40);
+            this.loadBtn.TabIndex = 5;
+            this.loadBtn.Text = "Load Information";
+            this.loadBtn.UseVisualStyleBackColor = true;
+            this.loadBtn.Click += new System.EventHandler(this.loadBtn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioUser);
+            this.groupBox1.Controls.Add(this.radioCust);
+            this.groupBox1.Location = new System.Drawing.Point(403, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(288, 116);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioUser
+            // 
+            this.radioUser.AutoSize = true;
+            this.radioUser.Location = new System.Drawing.Point(67, 73);
+            this.radioUser.Name = "radioUser";
+            this.radioUser.Size = new System.Drawing.Size(140, 21);
+            this.radioUser.TabIndex = 1;
+            this.radioUser.Text = "Users Information";
+            this.radioUser.UseVisualStyleBackColor = true;
+            // 
+            // radioCust
+            // 
+            this.radioCust.AutoSize = true;
+            this.radioCust.Checked = true;
+            this.radioCust.Location = new System.Drawing.Point(67, 33);
+            this.radioCust.Name = "radioCust";
+            this.radioCust.Size = new System.Drawing.Size(170, 21);
+            this.radioCust.TabIndex = 0;
+            this.radioCust.TabStop = true;
+            this.radioCust.Text = "Customers Information";
+            this.radioCust.UseVisualStyleBackColor = true;
             // 
             // DisconnectedMode
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(1098, 751);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.loadBtn);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "DisconnectedMode";
             this.Text = "DisconnectedMode";
+            this.Load += new System.EventHandler(this.DisconnectedMode_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button loadBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioUser;
+        private System.Windows.Forms.RadioButton radioCust;
     }
 }
--- Software-Engineering-Course-Project/DisconnectedMode.cs
+++ Software-Engineering-Course-Project/DisconnectedMode.cs
@@ -7,14 +7,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
 
 namespace Software_Engineering_Course_Project
 {
     public partial class DisconnectedMode : Form
     {
+        OracleDataAdapter oracleDataAdapter;
+        OracleCommandBuilder oracleCommandBuilder;
+        DataSet ds;
+
         public DisconnectedMode()
         {
             InitializeComponent();
+        }
+
+        private void DisconnectedMode_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loadBtn_Click(object sender, EventArgs e)
+        {
+            String connectionText = "Data Source=orcl; User Id=hr; Password=hr;";
+            String commandText = "";
+
+            if (radioCust.Checked) { commandText = "Select * From customers"; }
+            else if (radioUser.Checked) { commandText = "Select * From users"; }
+
+            ds = new DataSet();
+
+            oracleDataAdapter = new OracleDataAdapter(commandText, connectionText);
+            oracleDataAdapter.Fill(ds);
+            dataGridView.DataSource = ds.Tables[0];
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            oracleCommandBuilder = new OracleCommandBuilder(oracleDataAdapter);
+            oracleDataAdapter.Update(ds.Tables[0]);
+            MessageBox.Show("Table Updated");
         }
     }
 }
